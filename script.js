668ce99ee44080c567bf127bdb705f752c83b84b
alejandrabedolla <blancabedolla4@gmail.com>
2023-12-11 09:24:21 -0500

    almost done

---
+++ script.js
@@ -0,0 +1,4 @@
+const btn = document.querySelector('.button10');
+btn.addEventListener('click', function(){
+    window.open("https://www.linkedin.com/in/blanca-bedolla-lopez-75b350292/", " blanca's linkedIn")
+});
